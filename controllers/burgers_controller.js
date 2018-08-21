2722d2c22e0efce376279c6ae4e9fc0077dfb593
Adriana Capriles <adrianacapriles@gmail.com>
2018-08-21 19:37:54 -0400

    almost

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,35 +3,27 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function (req, res) {
-    burger.all(function (data) {
-        var hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject)
+router.get('/', function (req, res) {
+    res.redirect('/index');
+});
+
+router.get('/index', function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = { burgers: data };
+        res.render('index', hbsObject);
     });
 });
 
-router.post("/api/burgers", function (req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
-        res.json({ id: result.insertId });
+router.post('/burger/create', function (req, res) {
+    burger.insertOne(req.body.burger_name, function () {
+        res.redirect('/index');
     });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition ", condition);
-    burger.update(
-        {
-            devoured: req.body.devoured
-        }, condition, function (result) {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        }
-    );
+router.post('/burger/eat/:id', function (req, res) {
+    burger.updateOne(req.params.id, function () {
+        res.redirect('/index');
+    });
 });
 
 module.exports = router;
